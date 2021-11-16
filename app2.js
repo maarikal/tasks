71f00dce35b467dcdf0c6410a4b013a217f5a02d
Maarika Lukk <maarika.lukk@khk.ee>
2021-11-16 21:10:08 +0200

    Added app2.js as clean code, where are needed function (addTask, deleteTask and deleteTasks).

---
+++ app2.js
@@ -0,0 +1,53 @@
+// Koduõppe JS ehk siis nn puhtand
+
+const form = document.querySelector("form");
+const taskInput = document.querySelector("#task");
+const tasksList = document.querySelector(".collection")
+const deleteAllTasks = document.querySelector("#delete-tasks");
+
+form.addEventListener("submit", addTask);
+tasksList.addEventListener("click", deleteTask);
+deleteAllTasks.addEventListener("click", deleteTasks);
+
+
+function addTask(event) {
+    const task = taskInput.value;  // annab taskile väärtuse
+
+    // loome listi taskide lisamiseks ning lisame ul'i (listide kogumi) alla
+    const li = document.createElement("li");
+    li.className ="collection-item";
+    const taskText = document.createTextNode(task);
+    li.appendChild(taskText);
+
+    // loome lingitava elemendi ja lingi atribuudi
+    const link = document.createElement("a");
+    link.setAttribute("href", "#");
+    link.className = "secondary-content";
+    link.appendChild(document.createTextNode("X"));  // loome kustutamise nupu (X-i)
+    li.appendChild(link);
+
+    const ul = document.querySelector(".collection");
+    ul.appendChild(li);
+
+    taskInput.value = ""; // teeme Add taski kasti (ehk input välja) tühjaks pärast iga sisestust
+    event.preventDefault(); // kontrollib, kas vorm Submit töötab
+}
+
+
+function deleteTask(event) {
+    if(event.target.textContent == "X") {
+        if(confirm("Do you want to delete this task?")) {
+            event.target.parentElement.remove();
+        }
+    }
+}
+
+
+// saab kasutada ka tasksList.innterHTML = "", st kirjutab üle kõik sisetatud teksti, kuid see on ajamahukas protsess, removeChild teeb kiiremini
+function deleteTasks() {
+    if(confirm("Do you want to delete all tasks?")) {
+        while(tasksList.firstElementChild) {
+            tasksList.removeChild(tasksList.firstChild);
+        }
+    }
+}
